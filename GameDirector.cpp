6abf4e0d56e4ae1a66b0578edd95aa5bd88dbeab
Wilmer Forsman <wilmer.forsman@chasacademy.se>
2024-11-11 12:02:49 +0100

    second version

---
+++ GameDirector.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <random>
+
+int main()
+{
+    // game directors purpose is mainly to exist as the one that continues forward the games story through mostly randomizing which scenario to pick
+    // game director exists to call on other functions where it randomizes the scenario to play
+    return 0;
+}
