c71848fdb7b11ec128dc4248f7e0ba42cd2db2af
DoraTHH <erikerikssonsepost@gmail.com>
2024-11-18 13:16:47 +0100

    yeah

--- baseItems.h
+++ baseItems.h
@@ -3,12 +3,20 @@ using namespace std;
 
 class sword : public item
 {
+private:
+    int damage = 4;
 public:
     sword()
         : item("Sword", "My sword", 1) {}
 
-    int useItem(int attr) {
-        return attr + 4;
+    int useItem (int attr) {
+        //logik for damage and check whether the item can be used
+        if(attr > 1) {
+            return attr+damage;
+        } else {
+            return -1;
+        }
 
-    };
+    }
+
 };
