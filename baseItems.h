3497f20eb065ec88e6d1c247c0fae582054b4b3c
DoraTHH <erikerikssonsepost@gmail.com>
2024-11-18 13:57:46 +0100

    fuck i did it again

--- baseItems.h
+++ baseItems.h
@@ -1,3 +1,7 @@
+#if !defined(BASEITEMS_H)
+#define BASEITEMS_H
+
+#include <iostream>
 #include "item.h"
 using namespace std;
 
@@ -7,8 +11,45 @@ public:
     sword()
         : item("Sword", "My sword", 1) {}
 
+    // should pass dex, swords require a lot of dexterity to use
     int useItem(int attr) {
-        return attr + 4;
-
+        if (attr > 1) {
+            cout << "you swing your sword";
+            return attr + (rand() % 7);
+        } else {
+            cout << "you are too clumsy to use a sword effectively";
+            return -1;
+        }
     };
 };
+
+class bow : public item
+{
+public:
+    bow()
+        : item("Bow", "My bow", 1) {}
+
+    // should pass str, bows require a lot of strength to use
+    int useItem(int attr) {
+        if (attr > 1) {
+            cout << "you fire your bow";
+            return attr + (rand() % 7);
+        } else {
+            cout << "you are too weak to pull the bowstring";
+            return -1;
+        }
+    };
+};
+
+class healthPotion : public item
+{
+public:
+    healthPotion()
+        : item ("Health potion", "A slurry of herbs, reminds you of bog water", 1) {}
+
+    // returns an int
+    int useItem () {
+        return 4 + (rand() % 5);
+    };
+};
+#endif // BASEITEMS_H
