6be2e7f9cff767cd25be8934e6e335707a077dc4
DoraTHH <erikerikssonsepost@gmail.com>
2024-11-18 15:37:04 +0100

    added two spells

--- baseItems.h
+++ baseItems.h
@@ -5,6 +5,8 @@
 #include "item.h"
 using namespace std;
 
+/* WEAPONS */
+
 class sword : public item
 {
 private:
@@ -43,11 +45,48 @@ public:
     };
 };
 
+/* SPELLS */
+
+class fireBall : public item {
+    public:
+        fireBall()
+            : item ("Fire ball", "Opens a portal under satans ass", 1) {}
+    //should pass int, casting spells requires certain education
+    int useItem (int attr) {
+        if (attr > 1 && (rand() % 7) > 4) {
+            cout << "A portal opens and out spews flames";
+            return 25;
+        } else {
+            cout << "The spell fails for mysterious reasons";
+            return -1;
+        }
+    }
+};
+
+class fireBolt : public item {
+    public:
+        fireBolt()
+            : item ("Fire bolt", "Opens a portal next to an angry fire imp at a bar fight", 1) {}
+    //should pass int, casting spells requires certain education
+    int useItem (int attr) {
+        if (attr > 1 && (rand() % 7) > 2) {
+            cout << "A portal opens and out spews flames";
+            return 5;
+        } else {
+            cout << "The spell fails for mysterious reasons";
+            return -1;
+        }
+    }
+};
+
+
+
+/* POTIONS */
 class healthPotion : public item
 {
 public:
     healthPotion()
-        : item ("Health potion", "A slurry of herbs, reminds you of bog water", 1) {}
+        : item ("Health potion", "A brown slurry of herbs, reminds you of bog water", 1) {}
 
     // returns an int
     int useItem () {
@@ -59,7 +98,7 @@ class potentHealthPotion : public item
 {
 public:
     potentHealthPotion()
-        : item ("Potent health potion", "A black slurry of herbs, looks like tar and smells like shit", 1) {}
+        : item ("Potent health potion", "A black slurry of herbs, reminds you of rotting kelp", 1) {}
 
     // returns an int
     int useItem () {
